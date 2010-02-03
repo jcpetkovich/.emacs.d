1ac628b7382cb7c2f99109c1882ba011cab847a3
Jean-Christophe Petkovich <me@jcpetkovich.com>
2010-02-03 16:15:05 -0500

    fixed clojure weirdness in a crappy way

--- .emacs.d/configs/slime-options.el
+++ .emacs.d/configs/slime-options.el
@@ -9,4 +9,16 @@
   (require 'slime)
   (slime-setup))
 
+(defun myslime ()
+  "MY SLIME! DO IT NOOOOOOOOOOOW!"
+  (interactive)
+  (cd "~/src/clojure") ;dirty hack to satisfy clojure
+  (slime)
+  (cd "~/"))
 
+;; Clojure/slime classpath
+(setf swank-clojure-classpath '("/home/jcp/src/clojure/src/examples" "/home/jcp/src/clojure" "/home/jcp/src/clojure/src/" "/home/jcp/src/clojure/classes" "/home/jcp/.swank-clojure/clojure-1.1.0-master-20091202.150145-1.jar" "/home/jcp/.swank-clojure/clojure-contrib-1.1.0-master-20091212.205045-1.jar" "/home/jcp/.swank-clojure/swank-clojure-1.1.0.jar"))
+
+;; Fix slime hooks for viper-mode
+(add-hook 'slime-repl-mode-hook 'viper-change-state-to-vi)
+(add-hook 'slime-mode-hook 'viper-change-state-to-vi)
