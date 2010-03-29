b5207af6b064f97649f9164f2e1ae592b3a7339f
Jean-Christophe Petkovich <me@jcpetkovich.com>
2010-03-29 12:00:25 -0400

    added auctex

---
+++ .emacs.d/configs/auctex-config.el
@@ -0,0 +1,3 @@
+;(add-hook ')
+(add-hook 'LaTeX-mode-hook 'TeX-PDF-mode)
+(add-hook 'LaTeX-mode-hook 'flyspell-mode)
--- .emacs.d/configs/slime-options.el
+++ .emacs.d/configs/slime-options.el
@@ -22,3 +22,39 @@
 ;; Fix slime hooks for viper-mode
 (add-hook 'slime-repl-mode-hook 'viper-change-state-to-vi)
 (add-hook 'slime-mode-hook 'viper-change-state-to-vi)
+
+;;; all code in this function lifted from the clojure-mode function
+;;; from clojure-mode.el
+(defun clojure-font-lock-setup ()
+  (interactive)
+  (set (make-local-variable 'lisp-indent-function)
+       'clojure-indent-function)
+  (set (make-local-variable 'lisp-doc-string-elt-property)
+       'clojure-doc-string-elt)
+  (set (make-local-variable 'font-lock-multiline) t)
+ 
+  (add-to-list 'font-lock-extend-region-functions
+               'clojure-font-lock-extend-region-def t)
+ 
+  (when clojure-mode-font-lock-comment-sexp
+    (add-to-list 'font-lock-extend-region-functions
+                 'clojure-font-lock-extend-region-comment t)
+    (make-local-variable 'clojure-font-lock-keywords)
+    (add-to-list 'clojure-font-lock-keywords
+                 'clojure-font-lock-mark-comment t)
+    (set (make-local-variable 'open-paren-in-column-0-is-defun-start) nil))
+ 
+  (setq font-lock-defaults
+        '(clojure-font-lock-keywords ; keywords
+          nil nil
+          (("+-*/.<>=!?$%_&~^:@" . "w")) ; syntax alist
+          nil
+          (font-lock-mark-block-function . mark-defun)
+          (font-lock-syntactic-face-function
+           . lisp-font-lock-syntactic-face-function))))
+ 
+(add-hook 'slime-repl-mode-hook
+          (lambda ()
+            (font-lock-mode nil)
+            (clojure-font-lock-setup)
+            (font-lock-mode t)))
