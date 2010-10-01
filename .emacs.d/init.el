e59e948fd53f6bf0a8794fe29d58c6fe0cb46540
Jean-Christophe Petkovich <me@jcpetkovich.com>
2010-10-01 13:09:12 -0400

    fixed init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -162,15 +162,3 @@
 
 (put 'narrow-to-region 'disabled nil)
 
-
-
-
-;;; This was installed by package-install.el.
-;;; This provides support for the package system and
-;;; interfacing with ELPA, the package archive.
-;;; Move this code earlier if you want to reference
-;;; packages in your .emacs.
-(when
-    (load
-     (expand-file-name "~/.emacs.d/elpa/package.el"))
-  (package-initialize))
