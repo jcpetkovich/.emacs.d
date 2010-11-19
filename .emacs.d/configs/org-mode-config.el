367618a31e8c781689316d0fb318d34b356f7043
Jean-Christophe Petkovich <me@jcpetkovich.com>
2010-11-19 15:27:21 -0500

    added an org-mode-config.el file

---
+++ .emacs.d/configs/org-mode-config.el
@@ -0,0 +1,4 @@
+(setq remember-annotation-functions '(org-remember-annotation))
+(setq remember-handler-functions '(org-remember-handler))
+(add-hook 'remember-mode-hook 'org-remember-apply-template)
+
