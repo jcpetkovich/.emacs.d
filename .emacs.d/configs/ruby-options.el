8108ecac58717e1c4b87054f5ccf6740a9974f92
Jean Christophe Petkovich <me@jcpetkovich.com>
2011-01-09 21:41:39 -0500

    added rsense

--- .emacs.d/configs/ruby-options.el
+++ .emacs.d/configs/ruby-options.el
@@ -15,37 +15,45 @@
 (require 'icicles-rcodetools)
 (describe-function 'xmp)
 (describe-function 'comment-dwim)
-(describe-function 'rct-complete-symbol)
+;; (describe-function 'rct-complete-symbol)
 
 (add-hook 'ruby-mode-hook
           (lambda ()
             (define-key ruby-mode-map (kbd "\C-c\C-c\C-c") 'xmp)))
 
+;; (add-hook 'ruby-mode-hook
+;;           (lambda ()
+;;             (define-key ruby-mode-map (kbd "M-<tab>") 'rct-complete-symbol)))
+
+;; (icicle-define-command rct-complete-symbol--icicles
+;;                          "Perform ruby method and class completion on the text around point with icicles.
+;; C-M-RET shows RI documentation on each candidate.
+;; See also `rct-interactive'."
+
+;;                        (lambda (result)
+;;                          (save-excursion
+;;                            (search-backward pattern)
+;;                            (setq beg (point)))
+;;                          (delete-region beg end)
+;;                          (insert (car (split-string result)))) ;/function
+;;                        "rct-complete: "       ;prompt
+;;                        rct-method-completion-table
+;;                        nil nil pattern nil nil nil
+;;                        ((end (point)) beg
+;;                         (icicle-list-join-string "\t")
+;;                         (icicle-list-nth-parts-join-string "\t")
+;;                         (icicle-list-use-nth-parts '(1))
+;;                         (icicle-point-position-in-candidate 'input-end)
+;;                         pattern klass alist
+;;                         (icicle-candidate-help-fn
+;;                          (lambda (result)
+;;                            (ri (cdr (assoc result alist)))))) ;bindings
+;;                        (save-excursion (rct-exec-and-eval rct-complete-command-name "--completion-emacs-icicles")))
+
+(setq rsense-home "/home/jcp/src/ruby/rsense-0.3")
+(add-to-list 'load-path (concat rsense-home "/etc"))
+(require 'rsense)
+
 (add-hook 'ruby-mode-hook
           (lambda ()
-            (define-key ruby-mode-map (kbd "M-<tab>") 'rct-complete-symbol)))
-
-(icicle-define-command rct-complete-symbol--icicles
-                         "Perform ruby method and class completion on the text around point with icicles.
-C-M-RET shows RI documentation on each candidate.
-See also `rct-interactive'."
-
-                       (lambda (result)
-                         (save-excursion
-                           (search-backward pattern)
-                           (setq beg (point)))
-                         (delete-region beg end)
-                         (insert (car (split-string result)))) ;/function
-                       "rct-complete: "       ;prompt
-                       rct-method-completion-table
-                       nil nil pattern nil nil nil
-                       ((end (point)) beg
-                        (icicle-list-join-string "\t")
-                        (icicle-list-nth-parts-join-string "\t")
-                        (icicle-list-use-nth-parts '(1))
-                        (icicle-point-position-in-candidate 'input-end)
-                        pattern klass alist
-                        (icicle-candidate-help-fn
-                         (lambda (result)
-                           (ri (cdr (assoc result alist)))))) ;bindings
-                       (save-excursion (rct-exec-and-eval rct-complete-command-name "--completion-emacs-icicles")))
+            (local-set-key (kbd "C-c .") 'rsense-complete)))
