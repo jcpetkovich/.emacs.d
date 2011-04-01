de8add67de115be2b531f31a87a04acfbf9e03ea
Jean-Christophe Petkovich <me@jcpetkovich.com>
2011-04-01 13:17:56 -0400

    uniquify options

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -155,6 +155,7 @@
  '(tab-stop-list (quote (4 8 12 16 20 24 28 32 36 40 44 48 52 56 60)))
  '(tab-width 4)
  '(tags-revert-without-query t)
+ '(uniquify-buffer-name-style (quote post-forward) nil (uniquify))
  '(user-mail-address "me@jcpetkovich.com")
  '(vimpulse-want-C-i-like-Vim nil)
  '(viper-ex-style-motion nil)
