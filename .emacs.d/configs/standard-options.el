c3fae82cae288552bd896133083e99ecab043a9c
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2011-08-25 16:13:07 -0400

    added another require for the sake of archlinux

--- .emacs.d/configs/standard-options.el
+++ .emacs.d/configs/standard-options.el
@@ -37,6 +37,7 @@
 (require 'cl)
 (require 'ido) ; ido-mode for better buffer switching and file finding, C-f to return to normal style
 (require 'uniquify)
+(require 'cedet)
 
 ;; ============================================================= 
 ;; Uniquify
