94b668699f2c1d948fd9c3f8c08d7e9f1620db80
Jean-Christophe Petkovich <me@jcpetkovich.com>
2011-09-26 07:26:57 -0400

    changed up perl options a bit

--- .emacs.d/configs/perl-options.el
+++ .emacs.d/configs/perl-options.el
@@ -1,2 +1,18 @@
-;; (load "pde-load")
+(add-hook 'cperl-mode-hook (lambda ()
+                                (message "im disabling abbrev mode for pde")
+                                (abbrev-mode -1)))
+(load "pde-load")
 
+
+(add-hook 'cperl-mode-hook
+          (lambda ()
+            (vimpulse-local-set-key 'visual-state (kbd "<tab>") 'indent-for-tab-command)))
+
+;; (add-hook 'cperl-mode-hook
+;;           (lambda ()
+;;             (define-key cperl-mode-map (kbd "\M-;") ())))
+
+;; (add-to-list 'load-path "~/src/perl/sepia")
+;; (setq sepia-perl5lib (list (expand-file-name "~/src/perl/sepia/lib")))
+;; (defalias 'perl-mode 'sepia-mode)
+;; (require 'sepia)
