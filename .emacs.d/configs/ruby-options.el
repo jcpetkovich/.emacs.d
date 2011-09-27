03ef23e402cda507c7e40ca88baf2996d3479db1
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2011-09-27 16:41:24 -0400

    tweaking the ruby conf

--- .emacs.d/configs/ruby-options.el
+++ .emacs.d/configs/ruby-options.el
@@ -25,7 +25,7 @@
 
 (add-hook 'ruby-mode-hook
           (lambda ()
-            (define-key ruby-mode-map (kbd "\C-c\C-c\C-c") 'xmp)))
+            (define-key ruby-mode-map (kbd "\C-c\C-c") 'xmp)))
 
 (add-hook 'ruby-mode-hook
           (lambda ()
@@ -50,3 +50,75 @@
 (setq rsense-rurema-home "~/.ruby-reference-manual")
 
 
+(font-lock-add-keywords 'ruby-mode
+  '((".\\(each\\)\\_>" 1 font-lock-keyword-face)))
+
+(font-lock-add-keywords 'ruby-mode
+  '((".\\(collect\\)\\_>" 1 font-lock-keyword-face)))
+
+(font-lock-add-keywords 'ruby-mode
+  '((".\\(reject\\)\\_>" 1 font-lock-keyword-face)))
+
+(font-lock-add-keywords 'ruby-mode
+  '((".\\(select\\)\\_>" 1 font-lock-keyword-face)))
+
+(font-lock-add-keywords 'ruby-mode
+  '((".\\(inject\\)\\_>" 1 font-lock-keyword-face)))
+
+(font-lock-add-keywords 'ruby-mode
+  '((".\\(include\\)\\_>" 1 font-lock-keyword-face)))
+
+(font-lock-add-keywords 'ruby-mode
+  '((".\\(map\\)\\_>" 1 font-lock-keyword-face)))
+
+(font-lock-add-keywords 'ruby-mode
+  '((".\\(reduce\\)\\_>" 1 font-lock-keyword-face)))
+
+
+;; any?
+;; chunk
+;; collect
+;; collect_concat
+;; count
+;; cycle
+;; detect
+;; drop
+;; drop_while
+;; each_cons
+;; each_entry
+;; each_slice
+;; each_with_index
+;; each_with_object
+;; entries
+;; find
+;; find_all
+;; find_index
+;; first
+;; flat_map
+;; grep
+;; group_by
+;; include?
+;; inject
+;; map
+;; max
+;; max_by
+;; member?
+;; min
+;; min_by
+;; minmax
+;; minmax_by
+;; none?
+;; one?
+;; partition
+;; reduce
+;; reject
+;; reverse_each
+;; select
+;; slice_before
+;; sort
+;; sort_by
+;; take
+;; take_while
+;; to_a
+;; to_json
+;; zip
