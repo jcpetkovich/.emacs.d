65e0fadbae128ace216faf5e25ac89bd47fb3774
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2011-10-13 10:07:57 -0400

    fixed a random crash (it was in color-theme?)

--- .emacs.d/configs/python-options.el
+++ .emacs.d/configs/python-options.el
@@ -1,6 +1,7 @@
 ;; ============================================================= 
 ;; Alternative Python mode (i like this one better)
 ;; ============================================================= 
+(setq py-python-command "/usr/bin/python")
 (autoload 'python-mode "python-mode" "Python Mode." t)
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
