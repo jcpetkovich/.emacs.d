1fa19c9a31894805b39e2846d7b80c38b51c2af6
Jean Christophe Petkovich <me@jcpetkovich.com>
2012-01-04 00:07:40 -0500

    Added comment block command to R mode

--- .emacs.d/configs/r-options.el
+++ .emacs.d/configs/r-options.el
@@ -3,3 +3,7 @@
 (add-hook 'R-mode-hook
           (lambda ()
             (vimpulse-local-set-key 'visual-state (kbd "<tab>") 'indent-for-tab-command)))
+
+(add-hook 'R-mode-hook
+          (lambda ()
+            (local-set-key (kbd "M-;") 'comment-dwim)))
