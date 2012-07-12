25a85da5f0c8f2660926fbcb6655f91487a4a847
Jean Christophe Petkovich <me@jcpetkovich.com>
2012-07-12 16:41:45 -0400

    Fixes to ido-mode configs.

--- .emacs.d/configs/ido-mode-options.el
+++ .emacs.d/configs/ido-mode-options.el
@@ -1,3 +1,4 @@
-(eval-after-load "ido"
-  '(progn
-     (define-key ido-file-completion-map (kbd "C-w") 'ido-delete-backward-word-updir)))
+(add-hook 'ido-setup-hook
+          (lambda ()
+            (define-key ido-file-completion-map
+              (kbd "C-w") 'ido-delete-backward-word-updir)))
