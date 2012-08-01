536c45e173318dcc7ae1c45b50ed0e24e4b5b7a9
Jean-Christophe Petkovich <me@jcpetkovich.com>
2012-08-01 10:21:08 -0400

    Changed up the hippie expand config.

--- .emacs.d/configs/hippie-expand-options.el
+++ .emacs.d/configs/hippie-expand-options.el
@@ -6,11 +6,14 @@
                                          try-complete-file-name
                                          try-expand-all-abbrevs
                                          try-complete-lisp-symbol-partially
-                                         try-complete-lisp-symbol))
+                                         try-complete-lisp-symbol
+                                         try-expand-whole-kill
+                                         try-expand-line))
 
 (defun hippie-expand-lines ()
   (interactive)
   (let ((hippie-expand-try-functions-list '(try-expand-list
+                                            try-expand-list-all-buffers
                                             try-expand-line
                                             try-expand-line-all-buffers)))
     (hippie-expand nil)))
