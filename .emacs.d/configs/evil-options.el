92f11b0418446bfeec24193c221819c427770169
Jean Christophe Petkovich <me@jcpetkovich.com>
2012-08-18 15:10:20 -0400

    Removed some newer keybindings that didn't work out.

--- .emacs.d/configs/evil-options.el
+++ .emacs.d/configs/evil-options.el
@@ -17,10 +17,5 @@
                                  (kbd "C-+") 'increment-register
                                  (kbd "<f6>") 'browse-kill-ring
                                  (kbd "C-M-<backspace>") 'paredit-backward-delete
-                                 (kbd "<f7>") 'compile 
-                                 (kbd "M-n") 'move-cursor-next-pane
-                                 (kbd "M-p") 'move-cursor-previous-pane))
+                                 (kbd "<f7>") 'compile))
              '(normal insert))))
-
-
-
