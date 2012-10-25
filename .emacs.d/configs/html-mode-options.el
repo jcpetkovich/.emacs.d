398406d2f6c35f80a94f40b2c426cc38f330f4b3
Jean-Christophe Petkovich <me@jcpetkovich.com>
2012-10-25 10:39:13 -0400

    Added yas/expand to M-TAB to remove ambiguity.

--- .emacs.d/configs/html-mode-options.el
+++ .emacs.d/configs/html-mode-options.el
@@ -1,2 +1,8 @@
 
 (add-to-list 'auto-mode-alist '("\.mako$" . html-mode))
+
+(eval-after-load "evil"
+  '(progn
+     (evil-declare-key 'insert html-mode-map
+                       (kbd "M-<tab>") 'yas/expand)))
+
