c00def07af41214e830121abbdc197eaacd90573
Jean Christophe Petkovich <me@jcpetkovich.com>
2012-12-11 18:24:20 -0500

    Added hook to switch to bash style indentation for sh-mode when editing ebuilds.

--- .emacs.d/configs/ebuild-options.el
+++ .emacs.d/configs/ebuild-options.el
@@ -1,2 +1,6 @@
 
 (add-to-list 'auto-mode-alist '("\\.ebuild$" . sh-mode))
+
+(add-hook 'sh-mode-hook (lambda ()
+                          (when (string-match "\\.ebuild$" (buffer-file-name))
+                            (sh-set-shell "bash"))))
