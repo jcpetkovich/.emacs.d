ebe16e0e7c49182ed83ae5391820b334c30ab60a
Jean Christophe Petkovich <me@jcpetkovich.com>
2013-01-05 16:17:59 -0500

    Added solarized colour theme.

--- .emacs.d/configs/standard-options.el
+++ .emacs.d/configs/standard-options.el
@@ -1,17 +1,17 @@
-;; ============================================================= 
+;; =============================================================
 ;; Load Path
-;; ============================================================= 
+;; =============================================================
 (add-to-list 'load-path "/usr/share/emacs/site-lisp/color-theme")
 (add-to-list 'load-path "/home/jcp/jc-personal/site-lisp")
 (add-to-list 'load-path "/usr/lib64/chicken/5")
 
-;; ============================================================= 
+;; =============================================================
 ;; Custom Set Variables
-;; ============================================================= 
+;; =============================================================
 (setq-default indent-tabs-mode nil)     ;Tabs as spaces
 (setq org-hide-leading-stars     t
       org-odd-levels-only        t
-      inhibit-splash-screen      t          
+      inhibit-splash-screen      t
       lpr-command                "xpp" ; Add support for gui printing in linux
       display-time-day-and-date  t
       display-time-24hr-format   t
@@ -25,36 +25,47 @@
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 
 
-;; ============================================================= 
+;; =============================================================
 ;; Custom Set Functions
-;; ============================================================= 
+;; =============================================================
 (fset 'yes-or-no-p 'y-or-n-p)        ; Make acknowledging stuff faster
 
-;; ============================================================= 
+;; =============================================================
 ;; Mode Toggling
-;; ============================================================= 
+;; =============================================================
 (display-time)
 
-;; ============================================================= 
+;; =============================================================
 ;; Require Statements - that dont fit elsewhere
-;; ============================================================= 
+;; =============================================================
 (require 'cl)
 (require 'ido) ; ido-mode for better buffer switching and file finding, C-f to return to normal style
 (require 'uniquify)
 (require 'etags-update)
 
-;; ============================================================= 
+;; =============================================================
 ;; Winner Mode
-;; ============================================================= 
+;; =============================================================
 (winner-mode 1)
 
-;; ============================================================= 
+;; =============================================================
 ;; Color Theme
-;; ============================================================= 
-(require 'color-theme)
-(eval-after-load "color-theme"
- '(progn
-    (load "~/jc-personal/site-lisp/naquadah-theme")))
+;; =============================================================
+
+;; (load-theme 'solarized-dark t)
+(load "solarized-dark-theme")
+;; (require 'solarized)
+;; (eval-after-load "init"
+;;   '(progn
+;;      (deftheme solarized-light "The light variant of the Solarized colour theme")
+;;      (deftheme solarized-dark "The dark variant of the Solarized colour theme")
+;;      (defun light ()
+;;        (interactive)
+;;        (create-solarized-theme 'light 'solarized-light))
+;;      (defun dark ()
+;;        (interactive)
+;;        (create-solarized-theme 'dark 'solarized-dark))
+;;      (dark)))
 
 ;; =============================================================
 ;; Evil Mode
