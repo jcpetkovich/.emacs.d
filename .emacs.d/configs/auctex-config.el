af40f85247f72b806353a5f2382a827437313b11
Jean-Christophe Petkovich <me@jcpetkovich.com>
2013-03-04 09:44:01 -0500

    Wrote a tiny latex convenience function.

--- .emacs.d/configs/auctex-config.el
+++ .emacs.d/configs/auctex-config.el
@@ -21,3 +21,28 @@ line is naively added by `LaTeX-insert-item' when not already on
 an item line."
   (check-item-entry))
 
+
+(add-hook 'LaTeX-mode-hook
+          (lambda ()
+            (define-key LaTeX-mode-map (kbd "C-c DEL") 'delete-env-pair)))
+
+(defun delete-env-pair ()
+  "Deletes the \begin{} \end{} pair at point."
+  (interactive)
+  (save-excursion
+    (beginning-of-line)
+    (if (looking-at "\s*\\\\end")
+        (progn
+          (save-excursion
+            (LaTeX-find-matching-begin)
+            (beginning-of-line)
+            (kill-line)))
+      (if (looking-at "\s*\\\\begin")
+          (progn
+            (save-excursion
+              (end-of-line)
+              (LaTeX-find-matching-end)
+              (beginning-of-line)
+              (kill-line)))))
+    (beginning-of-line)
+    (kill-line)))
