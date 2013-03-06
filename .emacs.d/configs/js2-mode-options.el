16dc6910d3299af79c383b6cec224330a6978b14
Jean-Christophe Petkovich <me@jcpetkovich.com>
2013-03-06 11:24:47 -0500

    Updated js2-related stuff.

--- .emacs.d/configs/js2-mode-options.el
+++ .emacs.d/configs/js2-mode-options.el
@@ -2,3 +2,8 @@
 (add-to-list 'load-path "~/jc-public/site-lisp/js2-mode/")
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+(eval-after-load "js2-mode"
+  '(progn
+     (evil-declare-key 'normal 'js2-mode-map
+       (kbd "M-j") 'move-cursor-next-pane)))
