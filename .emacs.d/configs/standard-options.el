19e36a1d39ba1f46f47d10108f2f9f6fee4eef20
Jean Christophe Petkovich <me@jcpetkovich.com>
2013-03-06 18:40:43 -0500

    Added powerline.

--- .emacs.d/configs/standard-options.el
+++ .emacs.d/configs/standard-options.el
@@ -4,6 +4,7 @@
 (add-to-list 'load-path "/usr/share/emacs/site-lisp/color-theme")
 (add-to-list 'load-path "/home/jcp/jc-personal/site-lisp")
 (add-to-list 'load-path "/usr/lib64/chicken/5")
+(add-to-list 'load-path "~/jc-public/site-lisp/emacs-powerline")
 
 ;; =============================================================
 ;; Custom Set Variables
@@ -53,19 +54,52 @@
 ;; Color Theme
 ;; =============================================================
 
+(require 'powerline)
 (load "solarized-dark-theme")
 
 (custom-theme-set-faces
  'solarized-dark
  `(comint-highlight-prompt ((t (:foreground "#268bd2")))))
 
+(setq powerline-color1 "grey22")
+(setq powerline-color2 "grey40")
+
+(set-face-attribute 'mode-line nil
+                    :foreground "#fdf6e3"
+                    :background "#859900"
+                    :box nil)
+(set-face-attribute 'mode-line-inactive nil
+                    :box nil)
+
+(defun dark ()
+  (interactive)
+  (load-theme 'solarized-dark)
+  (setq powerline-color1 "grey22")
+  (setq powerline-color2 "grey40")
+
+  (set-face-attribute 'mode-line nil
+                      :foreground "#fdf6e3"
+                      :background "#859900"
+                      :box nil)
+  (set-face-attribute 'mode-line-inactive nil
+                      :box nil))
+
 (defun light ()
   (interactive)
-  (load "solarized-light-theme")
-(custom-theme-set-faces
- 'solarized-light
- `(comint-highlight-prompt ((t (:foreground "#268bd2")))))
-)
+  (load-theme 'solarized-light)
+  (custom-theme-set-faces
+   'solarized-light
+   `(comint-highlight-prompt ((t (:foreground "#268bd2")))))
+
+  (setq powerline-color1 "#657b83")
+  (setq powerline-color2 "#839496")
+
+  (set-face-attribute 'mode-line nil
+                      :foreground "#fdf6e3"
+                      :background "#859900"
+                      :box nil)
+  (set-face-attribute 'mode-line-inactive nil
+                      :box nil))
 
 ;; =============================================================
 ;; Evil Mode
