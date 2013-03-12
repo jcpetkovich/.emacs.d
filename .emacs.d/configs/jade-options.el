b5cec42742231c88cb920fa5879be3ba0e2e5bc5
Jean Christophe Petkovich <me@jcpetkovich.com>
2013-03-11 23:40:24 -0400

    Added jade mode.

---
+++ .emacs.d/configs/jade-options.el
@@ -0,0 +1,6 @@
+
+(add-to-list 'load-path "~/jc-public/site-lisp/jade-mode")
+(require 'sws-mode)
+(require 'jade-mode)    
+(add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
+(add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
