7262f3dc4dd70ad11e9750df05f3b1c266c9de90
Jean-Christophe Petkovich <me@jcpetkovich.com>
2013-03-20 11:33:23 -0400

    Added auto install to setup-package.el.

--- configs/setup-package.el
+++ configs/setup-package.el
@@ -1,8 +1,44 @@
 
 (require 'package)
-(add-to-list 'package-archives 
-    '("marmalade" .
-      "http://marmalade-repo.org/packages/"))
+
+(defvar marmalade '("marmalade" . "http://marmalade-repo.org/packages/"))
+(defvar gnu '("gnu" . "http://elpa.gnu.org/packages/"))
+(defvar melpa '("melpa" . "http://melpa.milkbox.net/packages/"))
+
+(add-to-list 'package-archives marmalade)
+(add-to-list 'package-archives melpa)
+
 (package-initialize)
+
+(unless (and (file-exists-p "~/.emacs.d/elpa/archives/marmalade")
+             (file-exists-p "~/.emacs.d/elpa/archives/gnu")
+             (file-exists-p "~/.emacs.d/elpa/archives/melpa"))
+  (package-refresh-contents))
+
+(defun packages-install (&rest packages)
+  (mapc (lambda (package)
+          (let ((name (car package))
+                (repo (cdr package)))
+            (when (not (package-installed-p name))
+              (let ((package-archives (list repo)))
+                (package-initialize)
+                (package-install name)))))
+        packages)
+  (package-initialize)
+  (delete-other-windows))
+
+(packages-install
+ (cons 'clojure-mode melpa)
+ (cons 'nrepl melpa)
+ (cons 'ac-nrepl melpa)
+ (cons 'auctex gnu)
+ (cons 'ess melpa)
+ (cons 'inf-ruby melpa)
+ (cons 'org melpa)
+ (cons 'paredit melpa)
+ (cons 'pymacs marmalade)
+ (cons 'python-mode marmalade)
+ (cons 'slime-js marmalade)
+ (cons 'slime-repl marmalade))
 
 (provide 'setup-package)
