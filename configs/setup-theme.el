322a2bc5ff7d202e22fbe089b81a7013e63f1e25
Jean-Christophe Petkovich <me@jcpetkovich.com>
2013-04-01 12:00:57 -0400

    Updated theme settings, now includes evil state and erc channels in powerline.

--- configs/setup-theme.el
+++ configs/setup-theme.el
@@ -52,6 +52,37 @@
 ;; =============================================================
 
 (require 'powerline)
+
+;; Setup modeline items
+(defun gcs-propertized-evil-mode-tag ()
+  (propertize evil-mode-line-tag 'font-lock-face
+              ;; Don't propertize if we're not in the selected buffer
+              (cond ((not (eq (current-buffer) (car (buffer-list)))) '())
+                    ((evil-emacs-state-p) '(:background "red"))
+                    (t '()))))
+
+(setq-default
+ mode-line-format
+ (list "%e"
+       '(:eval (concat
+                (gcs-propertized-evil-mode-tag)
+                (powerline-rmw            'left   nil  )
+                (powerline-buffer-id      'left   nil  powerline-color1  )
+                (powerline-major-mode     'left        powerline-color1  )
+                (powerline-minor-modes    'left        powerline-color1  )
+
+                (when (boundp 'erc-modified-channels-object)
+                  (powerline-make-left erc-modified-channels-object
+                                       powerline-color1))
+                (powerline-narrow         'left        powerline-color1  powerline-color2  )
+                (powerline-vc             'center                        powerline-color2  )
+                (powerline-make-fill                                     powerline-color2  )
+                (powerline-row            'right       powerline-color1  powerline-color2  )
+                (powerline-make-text      ":"          powerline-color1  )
+                (powerline-column         'right       powerline-color1  )
+                (powerline-percent        'right  nil  powerline-color1  )
+                (powerline-make-text      "  "    nil  )))))
+
 (load "solarized-dark-theme")
 
 (custom-theme-set-faces
