c8cad92103b628225b78a4f70c9c50757a5aa8fd
Jean-Christophe Petkovich <me@jcpetkovich.com>
2013-04-05 19:54:27 -0400

    Added erc conf.

---
+++ configs/setup-erc.el
@@ -0,0 +1,13 @@
+
+(setq erc-autojoin-channels-alist
+      '(("mozilla.org" "#rust")
+        ("freenode.net"
+         "#ruby"
+         "#python"
+         "#perl"
+         "#Node.js"
+         "#haskell"
+         "#clojure"
+         "#scheme"
+         "#lisp"
+         "#emacs")))
