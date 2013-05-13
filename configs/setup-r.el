f51448cf738e677cb03eb297c37a9ca453ebc584
Jean Christophe Petkovich <me@jcpetkovich.com>
2013-05-13 18:39:42 -0400

    Fixed keybindings in interactive ESS mode.

--- configs/setup-r.el
+++ configs/setup-r.el
@@ -4,10 +4,14 @@
 (eval-after-load "evil"
   '(progn
      (evil-declare-key 'visual ess-mode-map
-                       (kbd "<tab>") 'indent-for-tab-command)))
+       (kbd "<tab>") 'indent-for-tab-command
+       (kbd "C-d") 'evil-scroll-down)
+     (evil-declare-key 'normal inferior-ess-mode-map
+       (kbd "C-d") 'evil-scroll-down)))
 
 (add-hook 'R-mode-hook
           (lambda ()
             (local-set-key (kbd "M-;") 'comment-dwim)))
+
 
 (provide 'setup-r)
