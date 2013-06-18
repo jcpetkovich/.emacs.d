76215f9a29794fc2c61af325970eca4ec5755114
Jean Christophe Petkovich <me@jcpetkovich.com>
2013-06-18 17:36:56 -0400

    Added setup for grep and skewer, added a few snippets.

---
+++ snippets/cperl-mode/.yas-compiled-snippets.el
@@ -0,0 +1,9 @@
+;;; Compiled snippets and support files for `cperl-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'cperl-mode
+                     '(("arg" "my ($1) = @_;" "my () = @_;" nil nil nil nil nil nil)
+                       ("for" "for my \\$${1:item} (@${2:list}) {\n    $0\n}" "for my $item () {" nil nil nil nil nil nil)))
+
+
+;;; Do not edit! File generated at Mon May  6 20:27:36 2013
