c1975feb33e1742de08b05388f06ea4fd2cde77c
Jean-Christophe Petkovich <me@jcpetkovich.com>
2013-11-30 19:15:10 -0500

    Added latex snippets.

---
+++ snippets/latex-mode/.yas-compiled-snippets.el
@@ -0,0 +1,8 @@
+;;; Compiled snippets and support files for `latex-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'latex-mode
+                     '(("lst" "\\begin\\{lstlisting\\}[label=${1:listing}, language=${2:C}]\n$0\n\\end\\{lstlisting\\}" "lstlisting" nil nil nil nil nil nil)))
+
+
+;;; Do not edit! File generated at Sat Nov 30 19:14:07 2013
