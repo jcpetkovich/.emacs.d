d1d3e389af622df9abacb67256a41ccef63787e9
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2013-12-03 19:07:32 -0500

    Use eldoc mode.

--- configs/setup-elisp.el
+++ configs/setup-elisp.el
@@ -2,7 +2,8 @@
 (require-package 'elisp-slime-nav)
 
 (--each '(emacs-lisp-mode-hook ielm-mode-hook)
-  (add-hook it 'turn-on-elisp-slime-nav-mode))
+  (add-hook it 'turn-on-elisp-slime-nav-mode)
+  (add-hook it 'eldoc-mode))
 
 (eval-after-load "evil"
   '(progn
