10cebcbc98efcd071d2cf889dccb5ecfe548efda
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2013-12-10 22:24:10 -0500

    Added paredit to cider-repl-mode.

--- configs/setup-paredit.el
+++ configs/setup-paredit.el
@@ -12,10 +12,8 @@
 (let ((turn-on-paredit-mode (lambda () (paredit-mode 1))))
   ;; some hooks: lisp-mode-hook and scheme-mode-hook are recommended
   ;; in the paredit source code
-  (add-hook 'lisp-mode-hook turn-on-paredit-mode)
-  (add-hook 'scheme-mode-hook turn-on-paredit-mode)
-  (add-hook 'emacs-lisp-mode-hook turn-on-paredit-mode)
-  (add-hook 'slime-mode-hook turn-on-paredit-mode))
+  (--each '(lisp-mode-hook scheme-mode-hook emacs-lisp-mode-hook slime-mode-hook cider-repl-mode-hook)
+    (add-hook it turn-on-paredit-mode)))
 
 (defadvice paredit-close-round (after paredit-close-and-indent activate)
   (cleanup-buffer))
