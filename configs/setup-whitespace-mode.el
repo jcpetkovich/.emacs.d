e1c25800734c39e00850f6e69b6d6c12c357840a
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2013-12-12 20:45:23 -0500

    No cleaning up extra lines at BOF and EOF... I think...
    Well, maybe I'll change my mind about this, not sure yet.

--- configs/setup-whitespace-mode.el
+++ configs/setup-whitespace-mode.el
@@ -5,4 +5,10 @@
 ;;; impolite
 (global-whitespace-cleanup-mode)
 
+(set-default 'whitespace-cleanup-mode-only-if-initially-clean nil)
+
+;;; Don't remove extraneous space at the BOF and EOF please.
+(setq whitespace-style
+      (remove-if (lambda (q) (eq q 'empty)) whitespace-style))
+
 (provide 'setup-whitespace-mode)
