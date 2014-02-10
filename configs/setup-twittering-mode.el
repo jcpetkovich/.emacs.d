b63688b4799ee6fdf4be120f8aa8c871055db3d1
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2014-02-10 12:12:00 -0500

    Added twittering-mode.

---
+++ configs/setup-twittering-mode.el
@@ -0,0 +1,8 @@
+;; setup-twittering-mode.el - configure twittering mode
+
+(require-package 'twittering-mode)
+
+(setq twittering-icon-mode t
+      twittering-use-master-password t)
+
+(provide 'setup-twittering-mode)
