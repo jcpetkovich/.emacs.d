1bb7210cfcfe76525c17e101a49ee6ea391fb538
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2014-02-26 14:24:03 -0500

    Fixes to multiple cursors, new binding strategy.

--- configs/setup-multiple-cursors.el
+++ configs/setup-multiple-cursors.el
@@ -4,16 +4,54 @@
 
 (defvar mc--default-cmds-to-run-once nil)
 
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
-(global-set-key (kbd "C-c C-#") 'mc/insert-numbers)
+(global-set-key (kbd "A-C-v A-C-v") 'mc/edit-ends-of-lines)
+
+(global-set-key (kbd "A-C-#") 'mc/insert-numbers)
+;;; Next key -> A-C-t (p)
+;;  - `mc/markst-next-like-this`: Adds a cursor and region at the next part of the buffer forwards that matches the current region.
+(global-set-key (kbd "A-C-t") 'mc/markst-next-like-this)
+;;  - `mc/markst-next-word-like-this`: Like `mc/markst-next-like-this` but only for whole words.
+(global-set-key (kbd "A-C-S-t") 'mc/markst-next-word-like-this)
+;;  - `mc/markst-next-symbol-like-this`: Like `mc/markst-next-like-this` but only for whole symbols.
+(global-set-key (kbd "A-C-S-p") 'mc/markst-next-symbol-like-this)
+
+;;; Previous key -> A-C-s (f)
+;;  - `mc/markst-previous-like-this`: Adds a cursor and region at the next part of the buffer backwards that matches the current region.
+(global-set-key (kbd "A-C-s") 'mc/markst-previous-like-this)
+;;  - `mc/markst-previous-word-like-this`: Like `mc/markst-previous-like-this` but only for whole words.
+(global-set-key (kbd "A-C-S-s") 'mc/markst-previous-word-like-this)
+;;  - `mc/markst-previous-symbol-like-this`: Like `mc/markst-previous-like-this` but only for whole symbols.
+(global-set-key (kbd "A-C-S-f") 'mc/markst-previous-symbol-like-this)
+
+;; ### Mark many occurrences
+
+;;; Mark all key -> A-C-a (q)
+;;  - `mc/markst-all-like-this`: Marks all parts of the buffer that matches the current region.
+(global-set-key (kbd "A-C-a") 'mc/markst-all-like-this)
+;;  - `mc/markst-all-words-like-this`: Like `mc/markst-all-like-this` but only for whole words.
+(global-set-key (kbd "A-C-S-a") 'mc/markst-all-words-like-this)
+;;  - `mc/markst-all-symbols-like-this`: Like `mc/markst-all-like-this` but only for whole symbols.
+(global-set-key (kbd "A-C-S-q") 'mc/markst-all-symbols-like-this)
+
+;;; Mark all limited by key -> A-C-r (w) (x)
+;;  - `mc/mark-all-in-region`: Prompts for a string to match in the region, adding cursors to all of them.
+(global-set-key (kbd "A-C-r") 'mc/mark-all-in-region)
+;;  - `mc/mark-all-like-this-in-defun`: Marks all parts of the current defun that matches the current region.
+(global-set-key (kbd "A-C-w") 'mc/mark-all-like-this-in-defun)
+;;  - `mc/mark-all-words-like-this-in-defun`: Like `mc/mark-all-like-this-in-defun` but only for whole words.
+(global-set-key (kbd "A-C-S-w") 'mc/mark-all-words-like-this-in-defun)
+;;  - `mc/mark-all-symbols-like-this-in-defun`: Like `mc/mark-all-like-this-in-defun` but only for whole symbols.
+(global-set-key (kbd "A-C-x") 'mc/mark-all-symbols-like-this-in-defun)
+
+;;; Mark all god key -> A-C-c
+;;  - `mc/mark-all-like-this-dwim`: Tries to be smart about marking everything you want. Can be pressed multiple times.
+(global-set-key (kbd "A-C-c") 'mc/mark-all-like-this-dwim)
 
 (defvar my-mc-evil-previous-state nil)
 
 (defun my-mc-evil-switch-to-emacs-state ()
   (when (and (bound-and-true-p evil-mode)
+             (region-active-p)
              (not (memq evil-state '(insert emacs))))
     (setq my-mc-evil-previous-state evil-state)
     (let ((mark-before (mark))
