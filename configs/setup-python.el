1ab06df60116ecf4fd074bca89a77bb8c7b069d7
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2014-03-01 17:20:41 -0500

    Use ipython rather than python.

--- configs/setup-python.el
+++ configs/setup-python.el
@@ -13,4 +13,17 @@
                                     (kbd "M-.") 'jedi:goto-definition
                                     (kbd "M-,") 'jedi:goto-definition-pop-marker)))))
 
+;;; When ipython is available, prefer it.
+(when (executable-find "ipython")
+  (setq python-shell-interpreter "ipython"
+        python-shell-interpreter-args ""
+        python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+        python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+        python-shell-completion-setup-code
+        "from IPython.core.completerlib import module_completion"
+        python-shell-completion-module-string-code
+        "';'.join(module_completion('''%s'''))\n"
+        python-shell-completion-string-code
+        "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"))
+
 (provide 'setup-python)
