d8e5d90f377b9e13d2dea69376819545b572f358
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2014-05-18 02:07:20 -0400

    ipretty, pcomplete-extension, and new keybinding

--- configs/setup-elisp.el
+++ configs/setup-elisp.el
@@ -2,6 +2,11 @@
 (require 'setup-evil)
 (require-package 'elisp-slime-nav)
 (require-package 'info+)
+(require-package 'ipretty)
+(require-package 'pcomplete-extension)
+
+(global-set-key (kbd "C-h C-j") 'ipretty-last-sexp)
+(global-set-key (kbd "C-h C-k") 'ipretty-last-sexp-other-buffer)
 
 (--each '(emacs-lisp-mode-hook ielm-mode-hook)
   (add-hook it 'turn-on-elisp-slime-nav-mode)
@@ -10,5 +15,15 @@
 (--each '(normal insert)
   (evil-declare-key it emacs-lisp-mode-map
     (kbd "M-.") 'elisp-slime-nav-find-elisp-thing-at-point))
+
+;;; eval region
+
+(defun my-eval-region (start end)
+  (interactive "r")
+  (eval-region start end)
+  (deactivate-mark))
+
+(evil-declare-key 'visual global-map
+  (kbd ",") #'my-eval-region)
 
 (provide 'setup-elisp)
