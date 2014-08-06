d2f37492f296c304391a1f30a3338b00adeb14b5
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2014-08-06 11:36:44 -0400

    Use newer clojure completion source for ac.

--- configs/setup-clojure.el
+++ configs/setup-clojure.el
@@ -1,11 +1,13 @@
 
-(require-package 'ac-nrepl)
 (require-package 'clojure-mode)
 (require-package 'cider)
+(require-package 'ac-cider)
 
-(add-hook 'cider-repl-mode-hook 'ac-nrepl-setup)
-(add-hook 'cider-mode-hook 'ac-nrepl-setup)
+(add-hook 'cider-mode-hook 'ac-flyspell-workaround)
+(add-hook 'cider-mode-hook 'ac-cider-setup)
+(add-hook 'cider-repl-mode-hook 'ac-cider-setup)
+
 (eval-after-load "auto-complete"
-  '(add-to-list 'ac-modes 'cider-repl-mode))
+  '(add-to-list 'ac-modes 'cider-mode))
 
 (provide 'setup-clojure)
