752a2008b2accc65d814a32a922c804704778256
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2014-08-27 16:40:04 -0400

    Use emacs mode in cider popup buffers.

--- configs/setup-clojure.el
+++ configs/setup-clojure.el
@@ -3,11 +3,16 @@
 (require-package 'cider)
 (require-package 'ac-cider)
 
+(require 'setup-evil)
+
 (add-hook 'cider-mode-hook 'ac-flyspell-workaround)
 (add-hook 'cider-mode-hook 'ac-cider-setup)
 (add-hook 'cider-repl-mode-hook 'ac-cider-setup)
 
 (eval-after-load "auto-complete"
   '(add-to-list 'ac-modes 'cider-mode))
+
+(add-to-list 'evil-emacs-state-modes 'cider-stacktrace-mode)
+(add-to-list 'evil-emacs-state-modes 'cider-docview-mode)
 
 (provide 'setup-clojure)
