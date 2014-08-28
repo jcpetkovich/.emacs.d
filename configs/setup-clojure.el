ce35a7214edcef85de221dbb46e0c50746672d15
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2014-08-28 18:54:59 -0400

    Start switch to company mode.

--- configs/setup-clojure.el
+++ configs/setup-clojure.el
@@ -2,15 +2,9 @@
 (require-package 'clojure-mode)
 (require-package 'cider)
 (require-package 'ac-cider)
+(require-package 'company)
 
 (require 'setup-evil)
-
-(add-hook 'cider-mode-hook 'ac-flyspell-workaround)
-(add-hook 'cider-mode-hook 'ac-cider-setup)
-(add-hook 'cider-repl-mode-hook 'ac-cider-setup)
-
-(eval-after-load "auto-complete"
-  '(add-to-list 'ac-modes 'cider-mode))
 
 (add-to-list 'evil-emacs-state-modes 'cider-stacktrace-mode)
 (add-to-list 'evil-emacs-state-modes 'cider-docview-mode)
