622eec8d67cec6bf98a93c1def8e5063508e545e
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2014-09-04 14:00:57 -0400

    Reorganizing things in setup-theme

--- configs/setup-theme.el
+++ configs/setup-theme.el
@@ -1,60 +1,12 @@
 ;;; setup-theme.el - setup the look and feel of emacs
 
-;; =============================================================
-;; Custom Set Variables
-;; =============================================================
-(setq-default indent-tabs-mode nil      ;Tabs as spaces by default
-              save-place t)
-
-(setq inhibit-splash-screen      t
-      lpr-command                "xpp" ; Add support for gui printing in linux
-      display-time-day-and-date  nil
-      display-time-24hr-format   nil
-      backup-directory-alist
-      `(("." . ,(expand-file-name
-                 (concat user-emacs-directory "backups"))))
-      vc-make-backup-files t
-      save-interprogram-paste-before-kill t
-      save-place-file (concat user-emacs-directory "places")
-      mouse-wheel-scroll-amount '(1)
-      show-paren-style 'expression)
-
-;;; Make the frame title easy to search for among open windows
-(setq frame-title-format
-      (list
-       '(:eval (if buffer-file-name (buffer-file-name) (buffer-name)))))
-
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-
-
-;; =============================================================
-;; Custom Set Functions
-;; =============================================================
-(fset 'yes-or-no-p 'y-or-n-p)        ; Make acknowledging stuff faster
-
-;; =============================================================
-;; Mode Toggling
-;; =============================================================
-(display-time)
-
-;; =============================================================
-;; Require Statements - that dont fit elsewhere
-;; =============================================================
 (require 'cl)
 (require 'uniquify)
 (require 'saveplace)
 
 ;; =============================================================
-;; Winner Mode
-;; =============================================================
-(winner-mode 1)
-
-;; =============================================================
 ;; Color Theme
 ;; =============================================================
-
 (require-package 'smart-mode-line)
 (require-package 'moe-theme)
 (require 'moe-theme)
@@ -100,7 +52,6 @@
 ;; =============================================================
 ;; Line numbers
 ;; =============================================================
-
 (require-package 'nlinum)
 
 (global-nlinum-mode)
@@ -110,5 +61,42 @@
     (error (message "Unable to get font at this time.") 0)))
 
 (add-hook 'rcirc-mode-hook (lambda () (nlinum-mode -1)))
+
+
+;; =============================================================
+;; Custom Set Variables
+;; =============================================================
+(setq-default
+ indent-tabs-mode                    nil
+ save-place                          t
+ inhibit-splash-screen               t
+ lpr-command                         "xpp" ; default printing command
+ display-time-day-and-date           nil
+ display-time-24hr-format            nil
+ backup-directory-alist              `(("." . ,(expand-file-name (concat user-emacs-directory "backups"))))
+ vc-make-backup-files                t
+ save-interprogram-paste-before-kill t
+ save-place-file                     (concat user-emacs-directory "places")
+ mouse-wheel-scroll-amount           '(1)
+ show-paren-style                    'expression)
+
+;;; Make the frame title easy to search for among open windows
+(setq-default frame-title-format
+      (list
+       '(:eval (if buffer-file-name (buffer-file-name) (buffer-name)))))
+
+;;; No decorations please
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
+;;; Make acknowledging stuff faster
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;;; Display time
+(display-time)
+
+;;; Save window configurations
+(winner-mode 1)
 
 (provide 'setup-theme)
