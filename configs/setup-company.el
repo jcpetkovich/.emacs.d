f2a3439270f44396bc95b3bab5b4fc59e24e2483
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2014-09-04 11:37:30 -0400

    Trying to settle on company bindings, and figure out why they break sometimes.

--- configs/setup-company.el
+++ configs/setup-company.el
@@ -1,10 +1,12 @@
 (require-package 'company)
-
 (require 'company)
-(add-hook 'after-init-hook 'global-company-mode)
+(require 'setup-evil)
 
-(define-key company-active-map (kbd "C-n") 'company-select-next)
-(define-key company-active-map (kbd "C-p") 'company-select-previous)
-(define-key company-active-map (kbd "C-d") 'company-show-doc-buffer)
+(define-key company-active-map (kbd "C-h") 'help-command)
+(define-key company-active-map (kbd "C-w") 'backward-kill-word)
+(define-key company-active-map (kbd "C-l") 'company-show-location)
+(define-key company-active-map (kbd "M-1") nil)
+(define-key company-active-map (kbd "M-2") nil)
+(add-hook 'after-init-hook 'global-company-mode)
 
 (provide 'setup-company)
