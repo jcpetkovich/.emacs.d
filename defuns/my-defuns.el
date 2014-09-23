6edd0f683fb58de46db83beeee7cf580bcc1335f
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2014-09-23 09:46:53 -0400

    Refactor shrink-whitespace

--- defuns/my-defuns.el
+++ defuns/my-defuns.el
@@ -2,26 +2,6 @@
 ;;; ========================================
 ;;; My defuns
 ;;; ========================================
-(defun count-words-region (beginning end)
-  "Print number of words in the region."
-  (interactive "r")
-  (message "Counting words in region ... ")
-  (save-excursion
-    (let ((count 0))
-      (goto-char beginning)
-
-      (while (and (< (point) end)
-                  (re-search-forward "\\w+\\W*" end t))
-        (setq count (1+ count)))
-      (cond ((zerop count)
-             (message
-              "The region does NOT have any words."))
-            ((= 1 count)
-             (message
-              "The region has 1 word."))
-            (t
-             (message
-              "The region has %d words." count))))))
 
 ;; Toggle fullscreen mode
 (defun toggle-fullscreen ()
@@ -46,7 +26,7 @@
   (interactive)
   (call-interactively (key-binding (kbd "M-TAB"))))
 
-(defun fc-eval-and-replace ()
+(defun eval-and-replace ()
   "Replace the preceding sexp with its value."
   (interactive)
   (backward-kill-sexp)
@@ -57,64 +37,45 @@
            (insert (current-kill 0)))))
 
 (defun shrink-whitespace ()
-  "Remove white spaces around cursor to just one or none.
+  "Remove whitespace around cursor to just one or none.
 If current line contains non-white space chars, then shrink any
 whitespace char surrounding cursor to just one space.  If current
 line does not contain non-white space chars, then remove blank
 lines to just one."
   (interactive)
-  (let (cursor-point
-        line-has-meat-p  ; current line contains non-white space chars
-        spaceTabNeighbor-p
-        whitespace-begin whitespace-end
-        space-or-tab-begin space-or-tab-end
-        line-begin-pos line-end-pos)
-    (save-excursion
-      ;; todo: might consider whitespace as defined by syntax table,
-      ;; and also consider whitespace chars in unicode if syntax table
-      ;; doesn't already considered it.
-      (setq cursor-point (point))
-      (setq spaceTabNeighbor-p
-            (if (or (looking-at " \\|\t") (looking-back " \\|\t"))
-                t
-              nil))
-      (move-beginning-of-line 1)
-      (setq line-begin-pos (point))
-      (move-end-of-line 1)
-      (setq line-end-pos (point))
-      (setq line-has-meat-p
-            (if (< 0 (count-matches "[[:graph:]]" line-begin-pos line-end-pos))
-                t
-              nil))
-      (goto-char cursor-point)
-      (skip-chars-backward "\t ")
-      (setq space-or-tab-begin (point))
-      (skip-chars-backward "\t \n")
-      (setq whitespace-begin (point))
-      (goto-char cursor-point)
-      (skip-chars-forward "\t ")
-      (setq space-or-tab-end (point))
-      (skip-chars-forward "\t \n")
-      (setq whitespace-end (point)))
-    (if line-has-meat-p
-        (progn
-          (when spaceTabNeighbor-p
-            (delete-region space-or-tab-begin space-or-tab-end)
-            (insert " ")))
+  (cond ((just-one-space-p)
+         (delete-horizontal-space))
+        ((and (line-has-meat-p)
+              (or
+               (looking-at " \\|\t")
+               (looking-back " \\|\t")))
+         (just-one-space))
+        (t
+         (delete-blank-lines))))
 
-      (progn
-        (delete-blank-lines)))))
+(defun xor (a b)
+  (if a (not b) b))
+
+(defun just-one-space-p ()
+  "Returns true if there is only one space nearby."
+  (if (xor
+       (looking-at "^ ")
+       (looking-at "\\( \\|\t\\)[^ \t]")
+       (looking-back "[^ \t]\\( \\|\t\\)"))
+      t
+    nil))
 
 (defun line-has-meat-p ()
   "Returns `t' if line has any characters, `nil' otherwise."
-  (interactive)
-  (move-beginning-of-line 1)
-  (let ((line-begin-pos (point)))
-    (move-end-of-line 1)
-    (setq line-end-pos (point))
-    (if (< 0 (count-matches "[[:graph:]]" line-begin-pos line-end-pos))
-        t
-      nil)))
+  (save-excursion
+    (move-beginning-of-line 1)
+    (let ((line-begin-pos (point))
+          line-end-pos)
+      (move-end-of-line 1)
+      (setq line-end-pos (point))
+      (if (< 0 (count-matches "[[:graph:]]" line-begin-pos line-end-pos))
+          t
+        nil))))
 
 (defun grow-whitespace-around ()
   "Counterpart to shrink-whitespace, grow whitespace in a
