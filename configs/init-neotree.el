71d81a02f26a0afd592a474638586148fd94e646
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2014-09-23 09:55:23 -0400

    Moving configs to their new location.

--- configs/init-neotree.el
+++
@@ -1,7 +0,0 @@
-(require 'init-evil)
-
-;; (add-to-list 'load-path (s-concat user-emacs-directory "site-lisp/emacs-neotree"))
-;; (require 'neotree)
-;; (add-to-list 'evil-emacs-state-modes 'neotree-mode)
-
-(provide 'init-neotree)
