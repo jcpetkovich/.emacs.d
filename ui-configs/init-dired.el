db05f8cb5cb2a4883ffb5be8c44e0c85c1801f7a
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2014-09-23 16:39:41 -0400

    Req-package-ifying dired.

--- ui-configs/init-dired.el
+++ ui-configs/init-dired.el
@@ -1,68 +1,58 @@
-(require 'dired)
+;; init-dired.el - Configure dired.
 
-;; Make dired less verbose
-(require-package 'dired-details)
-(require 'dired-details)
-(require 'init-evil)
-(require 'find-dired)
+(req-package dired
+  :require (evil dired-details)
+  :config
+  (progn
+    (require 'dired-details)
+    (setq-default dired-details-hidden-string "--- "
+                  dired-guess-shell-alist-user '(("\\.pdf\\'" "zathura "))
+                  dired-dwim-target t)
+    (dired-details-install)
 
-;;; Use dired-x
-(add-hook 'dired-load-hook (lambda ()
-                             (load "dired-x")))
+    (defun dired/use-dired-x ()
+      (load "dired-x"))
 
-(setq-default dired-details-hidden-string "--- ")
-(dired-details-install)
+    (add-hook 'dired-load-hook 'dired/use-dired-x)
 
-(setq dired-guess-shell-alist-user
-      '(("\\.pdf\\'" "zathura ")))
+    (--each '(dired-do-rename
+              dired-do-copy
+              dired-create-directory
+              wdired-abort-changes)
+      (eval `(defadvice ,it (after revert-buffer activate)
+               (revert-buffer))))
 
-;; Move files between split panes
-(setq dired-dwim-target t)
+    (defun dired-back-to-start-of-files ()
+      (interactive)
+      (backward-char (- (current-column) 2)))
 
-;; Reload dired after making changes
-(--each '(dired-do-rename
-          dired-do-copy
-          dired-create-directory
-          wdired-abort-changes)
-  (eval `(defadvice ,it (after revert-buffer activate)
-           (revert-buffer))))
+    (defun dired-back-to-top ()
+      (interactive)
+      (beginning-of-buffer)
+      (dired-next-line 4))
 
-;; C-a is nicer in dired if it moves back to start of files
-(defun dired-back-to-start-of-files ()
-  (interactive)
-  (backward-char (- (current-column) 2)))
+    (defun dired-jump-to-bottom ()
+      (interactive)
+      (end-of-buffer)
+      (dired-next-line -1))
 
-(define-key dired-mode-map (kbd "C-a") 'dired-back-to-start-of-files)
-(define-key dired-mode-map (kbd "k") 'dired-do-delete)
+    (bind-keys :map dired-mode-map
+               ("C-a" . dired-back-to-start-of-files)
+               ("k" . dired-do-delete)
+               ("C-x C-k" . dired-do-delete))
 
-;; M-up is nicer in dired if it moves to the fourth line - the first file
-(defun dired-back-to-top ()
-  (interactive)
-  (beginning-of-buffer)
-  (dired-next-line 4))
+    (define-key dired-mode-map (vector 'remap 'beginning-of-buffer) 'dired-back-to-top)
+    (define-key dired-mode-map (vector 'remap 'smart-up) 'dired-back-to-top)
+    (define-key dired-mode-map (vector 'remap 'end-of-buffer) 'dired-jump-to-bottom)
+    (define-key dired-mode-map (vector 'remap 'smart-down) 'dired-jump-to-bottom)
 
-(define-key dired-mode-map (vector 'remap 'beginning-of-buffer) 'dired-back-to-top)
-(define-key dired-mode-map (vector 'remap 'smart-up) 'dired-back-to-top)
+    (eval-after-load "wdired"
+      '(progn
+         (define-key wdired-mode-map (kbd "C-a") 'dired-back-to-start-of-files)
+         (define-key wdired-mode-map (vector 'remap 'beginning-of-buffer) 'dired-back-to-top)
+         (define-key wdired-mode-map (vector 'remap 'end-of-buffer) 'dired-jump-to-bottom)))
 
-;; M-down is nicer in dired if it moves to the last file
-(defun dired-jump-to-bottom ()
-  (interactive)
-  (end-of-buffer)
-  (dired-next-line -1))
-
-(define-key dired-mode-map (vector 'remap 'end-of-buffer) 'dired-jump-to-bottom)
-(define-key dired-mode-map (vector 'remap 'smart-down) 'dired-jump-to-bottom)
-
-;; Delete with C-x C-k to match file buffers and magit
-(define-key dired-mode-map (kbd "C-x C-k") 'dired-do-delete)
-
-(eval-after-load "wdired"
-  '(progn
-     (define-key wdired-mode-map (kbd "C-a") 'dired-back-to-start-of-files)
-     (define-key wdired-mode-map (vector 'remap 'beginning-of-buffer) 'dired-back-to-top)
-     (define-key wdired-mode-map (vector 'remap 'end-of-buffer) 'dired-jump-to-bottom)))
-
-(evil-declare-key 'normal dired-mode-map
-  (kbd "n") 'evil-search-next)
+    (evil-declare-key 'normal dired-mode-map
+      (kbd "n") 'evil-search-next)))
 
 (provide 'init-dired)
