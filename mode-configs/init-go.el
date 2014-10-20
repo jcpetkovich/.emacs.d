58ddce786e8f86f6c4cafce82db9619673b0c1df
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2014-10-20 17:58:16 -0400

    Gofmt and compile command.

--- mode-configs/init-go.el
+++ mode-configs/init-go.el
@@ -10,7 +10,14 @@
     (bind-keys :map go-mode-map
                ("M-," . pop-tag-mark)
                ("M-." . godef-jump)
-               ("C-c C-d" . godoc-at-point))))
+               ("C-c C-d" . godoc-at-point))
+    (add-hook 'after-save-hook 'gofmt-before-save)
+
+    (add-hook 'go-mode-hook
+              (defun user-go/setup-compile ()
+                (if (not (string-match "go" compile-command))
+                    (set (make-local-variable 'compile-command)
+                         "go build -v && go test -v && go vet"))))))
 
 (req-package company-go
   :require go-mode)
