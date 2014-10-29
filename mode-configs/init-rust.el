bc71c40371636812b983d7d614bf20600a087496
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2014-10-29 18:24:57 -0400

    Use rust flycheck.

--- mode-configs/init-rust.el
+++ mode-configs/init-rust.el
@@ -3,4 +3,8 @@
 (req-package rust-mode
   :commands rust-mode)
 
+(req-package flycheck-rust
+  :require (rust-mode flycheck)
+  :config (add-hook 'flycheck-mode-hook 'flycheck-rust-setup))
+
 (provide 'init-rust)
