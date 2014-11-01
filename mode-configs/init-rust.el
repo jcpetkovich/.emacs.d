2cbcf6bd9c6f1960e0651ecd83da63d4043a14cc
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2014-10-31 22:25:09 -0400

    Fix some rust-mode + smartparens behaviour

--- mode-configs/init-rust.el
+++ mode-configs/init-rust.el
@@ -1,7 +1,21 @@
 ;; init-rust.el - Setup emacs for editing rust code.
 
+(req-package racer
+  :require company
+  :load-path ,(concat site-lisp-directory "/racer/editors/")
+  :init (progn
+          (setq-default racer-cmd (concat site-lisp-directory "/racer/bin/racer"))
+          (setq-default rust-srcpath (expand-file-name "~/test/rust/src/")))
+  :config
+  (require 'racer))
+
 (req-package rust-mode
-  :commands rust-mode)
+  :require (evil)
+  :commands rust-mode
+  :config
+  (progn
+    (evil-declare-key 'insert rust-mode-map
+      (kbd "'") 'self-insert-command)))
 
 (req-package flycheck-rust
   :require (rust-mode flycheck)
