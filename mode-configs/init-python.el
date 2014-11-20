8b11e53ab428c999a06bc564f5db2587e1ccd955
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2014-11-20 17:46:55 -0500

    Smart delete for python mode.

--- mode-configs/init-python.el
+++ mode-configs/init-python.el
@@ -16,9 +16,20 @@
   :commands python-mode
   :config
   (progn
+
+    (defun user-python/smart-delete ()
+      (interactive)
+      (let ((valid-pairs (sp--get-pair-list)))
+        (if (--any-p
+             (sp--looking-back (sp--strict-regexp-quote (cdr it)))
+             valid-pairs)
+            (sp-backward-delete-char)
+          (call-interactively 'python-indent-dedent-line-backspace))))
+
     (bind-keys :map python-mode-map
                ("M-." . jedi:goto-definition)
-               ("M-," . jedi:goto-definition-pop-marker))
+               ("M-," . jedi:goto-definition-pop-marker)
+               ("<backspace>" . user-python/smart-delete))
 
     ;; Fix evil bindings
     (-each '(normal insert)
