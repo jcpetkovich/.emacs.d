ef0eb8df0755e1775ba2502326fecb15f82e255a
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2015-01-03 12:32:25 -0500

    Clean up editing settings.

--- ui-configs/init-editing.el
+++ ui-configs/init-editing.el
@@ -1,22 +1,27 @@
 ;; init-editing.el - Configure editing related emacs settings.
 
 (setq-default
- load-prefer-newer                   t
- indent-tabs-mode                    nil
- save-place                          t
- lpr-command                         "xpp" ; default printing command
- backup-directory-alist              `(("." . ,(expand-file-name (concat user-emacs-directory "backups"))))
- make-backup-files                   t
- vc-make-backup-files                t
- save-interprogram-paste-before-kill t
- save-place-file                     (concat user-emacs-directory "places")
- tags-revert-without-query           t
- tags-table-list nil
- uniquify-buffer-name-style          'post-forward
+ ;; Editing
+ major-mode                  'org-mode
+ indent-tabs-mode            nil
+ sentence-end-double-space   nil
+ uniquify-buffer-name-style  'post-forward
+ save-place                  t
+ save-place-file             (concat user-emacs-directory "places")
+ lpr-command                 "xpp"
+ tags-revert-without-query   t
+ tags-table-list             nil
+
+ ;; Backups
+ backup-directory-alist `(("." . ,(expand-file-name (concat user-emacs-directory "backups"))))
+ make-backup-files      t
+ vc-make-backup-files   t
+
+ ;; Copy and Paste
  x-select-enable-clipboard           t
  x-select-enable-primary             t
- major-mode                          'org-mode
- sentence-end-double-space nil)
+ save-interprogram-paste-before-kill t
+ mouse-yank-at-point                 t)
 
 (require 'uniquify)
 (require 'saveplace)
