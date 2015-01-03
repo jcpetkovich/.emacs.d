f22e9ea307cba6a2f08b2bb4758fe6cdc381092a
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2015-01-03 13:01:27 -0500

    Use ruby-end in elixir mode.

--- mode-configs/init-elixir.el
+++ mode-configs/init-elixir.el
@@ -1,6 +1,14 @@
 ;; init-elixir.el - Setup emacs for editing elixir code.
 
-(req-package elixir-mode)
+(req-package elixir-mode
+  :config
+  (progn
+    (add-to-list 'elixir-mode-hook
+                 (defun auto-activate-ruby-end-mode-for-elixir-mode ()
+                   (set (make-variable-buffer-local 'ruby-end-expand-keywords-before-re)
+                        "\\(?:^\\|\\s-+\\)\\(?:do\\)")
+                   (set (make-variable-buffer-local 'ruby-end-check-statement-modifiers) nil)
+                   (ruby-end-mode +1)))))
 
 (req-package alchemist
   :require elixir-mode
