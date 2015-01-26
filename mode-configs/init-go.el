e8d08740e29e6a9142ad82d149de40c33a52b2bd
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2015-01-26 08:26:21 -0500

    Add keybindings for go.

--- mode-configs/init-go.el
+++ mode-configs/init-go.el
@@ -1,11 +1,34 @@
 ;; init-go.el - Setup emacs for editing go code.
 
 (req-package go-mode
+  :require evil-leader
   :config
   (progn
     (--each '(normal insert)
       (evil-declare-key it go-mode-map
         (kbd "M-.") 'godef-jump))
+
+    (evil-leader/set-key-for-mode 'go-mode
+      "moo" 'go-oracle-set-scope
+      "mo<" 'go-oracle-callers
+      "mo>" 'go-oracle-callees
+      "moc" 'go-oracle-peers
+      "mod" 'go-oracle-definition
+      "mof" 'go-oracle-freevars
+      "mog" 'go-oracle-callgraph
+      "moi" 'go-oracle-implements
+      "mop" 'go-oracle-pointsto
+      "mor" 'go-oracle-referrers
+      "mos" 'go-oracle-callstack
+      "mot" 'go-oracle-describe
+      "mdp"  'godoc-at-point
+      "mig"  'go-goto-imports
+      "mia"  'go-import-add
+      "mir"  'go-remove-unused-imports
+      "mpb"  'go-play-buffer
+      "mpr"  'go-play-region
+      "mpd"  'go-download-play
+      "mgg"   'godef-jump)
 
     (bind-keys :map go-mode-map
                ("M-," . pop-tag-mark)
