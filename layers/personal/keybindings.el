09b8b0d047e53ade124a33ce4f9b506fe0bbff6d
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2015-01-29 09:05:51 -0500

    Add keybindings for personal layer.

---
+++ layers/personal/keybindings.el
@@ -0,0 +1,17 @@
+;;; keybindings.el --- Spacemacs Layer key-bindings File
+;;
+;; Copyright (c) 2012-2014 Jean-Christophe Petkovich
+;; Copyright (c) 2014-2015 Jean-Christophe Petkovich & Contributors
+;;
+;; Author: Jean-Christophe Petkovich <jcpetkovich@gmail.com>
+;; URL: https://github.com/jcpetkovich/.emacs.d
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(bind-keys
+ ("M-1" . delete-other-windows)
+ ("M-!" . delete-window)
+ ("M-2" . split-window-vertically)
+ ("M-@" . split-window-horizontally))
