249d4ae2dad170c01c687fddcc25bf32aa4119e3
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2015-01-29 19:30:20 -0500

    Hippie expand and dired configs.

--- layers/personal/extensions.el
+++ layers/personal/extensions.el
@@ -21,6 +21,9 @@
     simple
     mu4e
     rcirc
+    hippie-expand
+    dired
+    wdired
     )
   "List of all extensions to load after the packages.")
 
@@ -28,3 +31,87 @@
   (use-package simple
     :config (setq-default shell-file-name (executable-find "bash"))))
 
+(defun personal/init-hippie-expand ()
+  (use-package hippie-exp
+    :bind (("M-/" . hippie-expand)
+           ("M-?" . hippie-expand-lines))
+    :init
+    (progn
+      (defun hippie-expand-lines ()
+        (interactive)
+        (let ((hippie-expand-try-functions-list '(try-expand-list
+                                                  try-expand-list-all-buffers
+                                                  try-expand-line
+                                                  try-expand-line-all-buffers)))
+          (hippie-expand nil))))
+
+    :config
+    (progn
+      (setq-default hippie-expand-dabbrev-skip-space t
+                    hippie-expand-try-functions-list
+                    '(try-expand-dabbrev
+                      try-expand-dabbrev-all-buffers
+                      try-expand-dabbrev-from-kill
+                      try-complete-file-name-partially
+                      try-complete-file-name
+                      try-expand-all-abbrevs
+                      try-complete-lisp-symbol-partially
+                      try-complete-lisp-symbol
+                      try-expand-whole-kill
+                      try-expand-line))
+
+      (defadvice he-substitute-string (after completion/he-paredit-fix activate)
+        "remove extra paren when expanding line in paredit"
+        (if (and paredit-mode (equal (substring str -1) ")"))
+            (progn (backward-delete-char 1) (forward-char)))))))
+
+(defun personal/init-wdired ()
+  (use-package wdired))
+
+(defun personal/init-dired ()
+  (use-package dired
+    :config
+    (progn
+      (require 'wdired)
+      (setq-default dired-hide-details-hide-symlink-targets nil
+                    dired-guess-shell-alist-user '(("\\.pdf\\'" "zathura "))
+                    dired-dwim-target t)
+
+      (defun dired/use-dired-x ()
+        (load "dired-x"))
+
+      (add-hook 'dired-load-hook 'dired/use-dired-x)
+      (add-hook 'dired-mode-hook (lambda () (dired-hide-details-mode 1)))
+
+      (defun dired-back-to-start-of-files ()
+        (interactive)
+        (backward-char (- (current-column) 2)))
+
+      (defun dired-back-to-top ()
+        (interactive)
+        (beginning-of-buffer)
+        (dired-next-line 3))
+
+      (defun dired-jump-to-bottom ()
+        (interactive)
+        (end-of-buffer)
+        (dired-next-line -1))
+
+      (bind-keys :map dired-mode-map
+                 ("C-a" . dired-back-to-start-of-files)
+                 ("k" . dired-do-delete)
+                 ("C-x C-k" . dired-do-delete))
+
+      (bind-keys :map dired-mode-map
+                 ([remap beginning-of-buffer] . dired-back-to-top)
+                 ([remap smart-up] . dired-back-to-top)
+                 ([remap end-of-buffer] . dired-jump-to-bottom)
+                 ([remap smart-down] . dired-jump-to-bottom))
+
+      (bind-keys :map wdired-mode-map
+                 ("C-a" . dired-back-to-start-of-files)
+                 ([remap beginning-of-buffer] . dired-back-to-top)
+                 ([remap end-of-buffer] . dired-jump-to-bottom))
+
+      (evil-declare-key 'normal dired-mode-map
+        (kbd "n") 'evil-search-next))))
