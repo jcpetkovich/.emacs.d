399b2c4557bac4f94338337d56d76465898aa1de
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2015-01-29 19:26:35 -0500

    Fix some bugs in the multiple-cursors layer.

--- layers/multiple-cursors/packages.el
+++ layers/multiple-cursors/packages.el
@@ -13,6 +13,7 @@
 (defvar multiple-cursors-packages
   '(
     multiple-cursors
+    expand-region
     )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
@@ -20,96 +21,18 @@ which require an initialization must be listed explicitly in the list.")
 (defvar multiple-cursors-excluded-packages '()
   "List of packages to exclude.")
 
-(defvar multiple-cursors/evil-prev-state nil)
-(defvar multiple-cursors/mark-was-active nil)
-
-(defun multiple-cursors/evil-visual-or-normal-p ()
-  "True if evil mode is enabled, and we are in normal or visual mode."
-  (and (bound-and-true-p evil-mode)
-       (not (memq evil-state '(insert emacs)))))
-
-(defun multiple-cursors/switch-to-emacs-state ()
-  (when (multiple-cursors/evil-visual-or-normal-p)
-
-    (setq multiple-cursors/evil-prev-state evil-state)
-
-    (when (region-active-p)
-      (setq multiple-cursors/mark-was-active t))
-
-    (let ((mark-before (mark))
-          (point-before (point)))
-
-      (evil-emacs-state 1)
-
-      (when (or multiple-cursors/mark-was-active (region-active-p))
-        (goto-char point-before)
-        (set-mark mark-before)))))
-
-(defun multiple-cursors/back-to-previous-state ()
-  (when multiple-cursors/evil-prev-state
-    (unwind-protect
-        (case multiple-cursors/evil-prev-state
-          ((normal visual) (evil-force-normal-state))
-          (t (message "Don't know how to handle previous state: %S"
-                      multiple-cursors/evil-prev-state)))
-      (setq multiple-cursors/evil-prev-state nil)
-      (setq multiple-cursors/mark-was-active nil))))
-
-(defun multiple-cursors/rectangular-switch-state ()
-  (if rectangular-region-mode
-      (multiple-cursors/switch-to-emacs-state)
-    (setq multiple-cursors/evil-prev-state nil)))
-
-(defadvice mc/edit-lines (before change-point-by-1 disable)
-  " When running edit-lines, point will return (position + 1) as
- a result of how evil deals with regions"
-  (when (multiple-cursors/evil-visual-or-normal-p)
-    (if (> (point) (mark))
-        (goto-char (1- (point)))
-      (push-mark (1- (mark))))))
-
-(defun multiple-cursors/expand-or-mark-next-symbol ()
-  (interactive)
-  (if (not (region-active-p))
-      (er/mark-symbol)
-    (let ((current-symbol (thing-at-point 'symbol))
-          (current-region (car (mc/region-strings))))
-      (if (string-equal current-symbol current-region)
-          (call-interactively #'mc/mark-next-symbol-like-this)
-        (call-interactively #'mc/mark-next-like-this)))))
-
-(defun multiple-cursors/expand-or-mark-next-word ()
-  (interactive)
-  (if (not (region-active-p))
-      (er/mark-word)
-    (call-interactively #'mc/mark-next-like-this)))
-
-
-(defun multiple-cursors/enable-compat ()
-  (add-hook 'multiple-cursors-mode-enabled-hook
-            'multiple-cursors/switch-to-emacs-state)
-  (add-hook 'multiple-cursors-mode-disabled-hook
-            'multiple-cursors/back-to-previous-state)
-  (add-hook 'rectangular-region-mode-hook 'multiple-cursors/rectangular-switch-state)
-
-  (ad-enable-advice 'mc/edit-lines 'before 'change-point-by-1))
-
-(defun multiple-cursors/disable-compat ()
-  (remove-hook 'multiple-cursors-mode-enabled-hook
-               'multiple-cursors/switch-to-emacs-state)
-  (remove-hook 'multiple-cursors-mode-disabled-hook
-               'multiple-cursors/back-to-previous-state)
-  (remove-hook 'rectangular-region-mode-hook 'multiple-cursors/rectangular-switch-state)
-
-  (ad-disable-advice 'mc/edit-lines 'before 'change-point-by-1))
-
 ;; For each package, define a function multiple-cursors/init-<package-multiple-cursors>
 (defun multiple-cursors/init-multiple-cursors ()
   "Initialize multiple cursors"
   (use-package multiple-cursors
-    :config
-    (require 'mc-mark-more)
-    (multiple-cursors/enable-compat)))
+    :init
+    (progn
+      (require 'mc-mark-more)
+      (multiple-cursors/enable-compat))))
+
+(defun multiple-cursors/init-expand-region ()
+  (use-package expand-region
+    :commands (er/mark-word er/mark-symbol)))
 
 ;; Often the body of an initialize function uses `use-package'
 ;; For more info on `use-package', see readme:
