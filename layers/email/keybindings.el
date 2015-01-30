82691f0fb67b44bf95dd2433e13fa8690ce86bdb
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2015-01-29 19:28:20 -0500

    Added email layer.

---
+++ layers/email/keybindings.el
@@ -0,0 +1,13 @@
+;;; extensions.el --- email Layer keybindings File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(evil-leader/set-key "ae" 'mu4e)
