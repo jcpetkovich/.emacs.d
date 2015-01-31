7c792d9e59c7afb1210d755a2d61f165c9f2b267
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2015-01-31 14:10:06 -0500

    Start mc lists from scratch.

--- .mc-lists.el
+++ .mc-lists.el
@@ -3,187 +3,33 @@
 
 (setq mc/cmds-to-run-for-all
       '(
-        LaTeX-babel-insert-hyphen
-        LaTeX-environment
-        TeX-insert-backslash
-        TeX-insert-quote
         TeX-insert-sub-or-superscript
-        TeX-newline
-        backward-kill-sentence
-        backward-sentence
-        backward-sexp
-        beginning-of-buffer
-        c-beginning-of-statement
-        c-electric-colon
-        c-electric-delete-forward
-        c-electric-pound
-        c-electric-semi&comma
-        c-electric-slash
-        c-electric-star
-        coffee-dedent-line-backspace
-        comment-dwim-2
-        cperl-electric-backspace
-        cperl-electric-brace
-        cperl-electric-lbrace
-        cperl-electric-paren
-        cperl-electric-rparen
-        cperl-electric-semi
-        cperl-electric-terminator
-        dabbrev-completion
-        dabbrev-expand
-        delete-horizontal-space
-        dired-back-to-start-of-files
-        dired-copy-filename-as-kill
-        dired-do-delete
-        dired-flag-file-deletion
-        dired-jump-to-bottom
-        dired-mark
-        dired-next-line
-        dired-previous-line
-        dired-unmark
-        electric-newline-and-maybe-indent
-        ess-noweb-indent-line
-        ess-smart-S-assign
-        ess-smart-comma
-        ess-smart-underscore
-        ess-yank
-        eval-and-replace
-        eval-last-sexp
-        evil-ace-jump-word-mode
-        evil-append
-        evil-backward-WORD-begin
-        evil-backward-char
-        evil-backward-sentence
-        evil-backward-word-begin
-        evil-change
-        evil-copy-from-below
-        evil-delete
-        evil-digit-argument-or-evil-beginning-of-line
-        evil-find-char
-        evil-find-char-to
-        evil-forward-WORD-end
-        evil-forward-char
-        evil-forward-word-begin
-        evil-forward-word-end
-        evil-insert
         evil-insert-state
         evil-lisp-state-sp-forward-barf-sexp
-        evil-lisp-state-sp-unwrap-sexp
+        evil-lisp-state-sp-forward-slurp-sexp
         evil-lisp-state-wrap
-        evil-next-line
         evil-normal-state
-        evil-paste-after
-        evil-previous-line
-        evil-ret
-        evil-substitute
-        fc-eval-and-replace
-        fill-paragraph
-        forward-sexp
-        helm-M-x
-        indent-for-tab-command
-        jedi:dot-complete
-        js2-insert-and-indent
-        keyboard-quit
-        kill-region-or-backward-word
-        kill-sexp
-        lisp-state-beginning-of-sexp
         lisp-state-forward-symbol
-        markdown-dedent-or-delete
-        markdown-exdent-or-delete
-        message-beginning-of-line
-        new-line-dwim
-        org-beginning-of-line
-        org-ctrl-c-ctrl-c
-        org-end-of-line
-        org-force-self-insert
-        org-insert-heading
-        org-kill-line
-        org-meta-return
-        org-metaleft
         org-metaright
-        org-return-indent
         org-self-insert-command
         org-shiftright
-        org-yank
-        orgtbl-hijacker-command-100
-        orgtbl-hijacker-command-102
-        orgtbl-hijacker-command-27
-        orgtbl-self-insert-command
-        paredit-backward
-        paredit-backward-up
         paredit-close-round
-        paredit-comment-dwim
-        paredit-doublequote
-        paredit-forward
-        paredit-forward-barf-sexp
         paredit-forward-delete
-        paredit-forward-down
-        paredit-forward-kill-word
         paredit-forward-slurp-sexp
-        paredit-forward-up
         paredit-kill
         paredit-newline
         paredit-open-round
-        paredit-open-square
-        paredit-semicolon
-        paredit-wrap-round
-        python-indent-dedent-line-backspace
-        python-indent-electric-colon
-        reindent-then-newline-and-indent
-        repeat
-        sgml-slash
-        sh-assignment
-        shrink-whitespace
-        snakeify-current-word
-        sp--self-insert-command
-        sp-backward-delete-char
-        sp-backward-down-sexp
         sp-backward-kill-word
-        sp-backward-sexp
-        sp-backward-up-sexp
-        sp-delete-char
-        sp-down-sexp
-        sp-forward-barf-sexp
-        sp-forward-sexp
+        sp-backward-symbol
+        sp-end-of-sexp
         sp-forward-slurp-sexp
-        sp-kill-hybrid-sexp
-        sp-kill-sexp
         sp-kill-word
-        sp-up-sexp
-        transpose-params
-        transpose-sexps
-        transpose-words
-        undefined
-        user-python/smart-delete
-        personal/kill-region-or-backward-word
         yas-expand
         ))
 
 (setq mc/cmds-to-run-once
       '(
-        describe-key
-        dired-do-flagged-delete
-        edebug-continue-mode
-        edebug-next-mode
-        eval-expression
-        evil-emacs-state
-        evil-scroll-line-up
-        evil-scroll-line-down
-        handle-switch-frame
-        helm-confirm-and-exit-minibuffer
-        helm-descbinds
-        helm-find-files
-        helm-show-kill-ring
-        keyboard-escape-quit
-        mc-expand-or-mark-next-symbol
-        mc-expand-or-mark-next-word
-        move-cursor-previous-pane
-        org-update-statistics-cookies
-        orgtbl-hijacker-command-105
-        overwrite-mode
-        revert-buffer
-        toggle-input-method
-        user-mc/expand-or-mark-next-symbol
-        user-mc/expand-or-mark-next-word
-        personal/move-cursor-previous-pane
+        elisp-slime-nav-find-elisp-thing-at-point
+        evil-escape-emacs-state
+        multiple-cursors/expand-or-mark-next-symbol
         ))
