f683795b453f94f33870e86b98b81d8933c1836b
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2015-02-01 14:37:45 -0500

    Smarter latex behaviour.

--- layers/personal/packages.el
+++ layers/personal/packages.el
@@ -145,7 +145,59 @@ which require an initialization must be listed explicitly in the list.")
     :defer t
     :config
     (progn
-      (add-hook 'LaTeX-mode-hook 'turn-on-smartparens-mode))))
+
+      (evil-leader/set-key-for-mode 'latex-mode
+        "md" 'personal/tex-delete-env-pair)
+
+      (add-hook 'LaTeX-mode-hook 'reftex-mode)
+      (add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
+      (add-hook 'LaTeX-mode-hook 'orgtbl-mode)
+      (add-hook 'LaTeX-mode-hook 'turn-on-smartparens-mode)
+
+      (push '("zathura" "zathura -s -x \"emacsclient --no-wait +%%{line} %%{input}\" %s.pdf")
+            TeX-view-program-list)
+      (push '(output-pdf "zathura")
+            TeX-view-program-selection)
+
+      (defun personal/tex-check-item-entry ()
+        "This function is meant to be used as advice for the
+`LaTeX-insert-item' function. The purpose behind this is to delete
+the extra blank line that is naively added by `LaTeX-insert-item'
+when not already on an item line."
+        (interactive)
+        (save-excursion
+          ;; Backward one line, check if it happened if the line we're
+          ;; looking is empty, delete it
+          (if (and (= (forward-line -1) 0)
+                   (looking-at "^\\s-*$"))
+              (kill-line))))
+
+      (defadvice LaTeX-insert-item (after remove-whitespace-first-item activate)
+        "This advice is meant to fix the issue where an extra blank
+line is naively added by `LaTeX-insert-item' when not already on
+an item line."
+        (personal/tex-check-item-entry))
+
+      (defun personal/tex-delete-env-pair ()
+        "Deletes the \begin{} \end{} pair at point."
+        (interactive)
+        (save-excursion
+          (beginning-of-line)
+          (if (looking-at "\s*\\\\end")
+              (progn
+                (save-excursion
+                  (LaTeX-find-matching-begin)
+                  (beginning-of-line)
+                  (kill-line)))
+            (if (looking-at "\s*\\\\begin")
+                (progn
+                  (save-excursion
+                    (end-of-line)
+                    (LaTeX-find-matching-end)
+                    (beginning-of-line)
+                    (kill-line)))))
+          (beginning-of-line)
+          (kill-line))))))
 
 (defun personal/init-multiple-cursors ()
   (use-package multiple-cursors
