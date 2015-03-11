74ed9923822f045ac31ff2cff8568d5c4ededea4
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2015-03-11 14:47:11 -0400

    Added advice for org publishing.

--- layers/blog/packages.el
+++ layers/blog/packages.el
@@ -34,35 +34,21 @@ which require an initialization must be listed explicitly in the list.")
                                 op/repository-directory
                                 " root@ptk.io:/var/www/localhost/htdocs") nil))
 
-      (defun op/render-content (&optional template param-table)
-        "My patched version of op/render-content to use org as a backend for css."
-        (mustache-render
-         (op/get-cache-create
-          (if template
-              (intern (replace-regexp-in-string "\\.mustache$" "-template" template))
-            :post-template)
-          (message (concat "Read " (or template "post.mustache") " from file"))
-          (file-to-string (concat op/template-directory
-                                  (or template "post.mustache"))))
-         (or param-table
-             (ht ("title" (or (op/read-org-option "TITLE") "Untitled"))
-                 ("content" (org-export-as 'html nil nil nil nil))))))
-
       (defvar blog/org-inline-css-hook-called nil)
 
       (defun blog/org-inline-css-hook (exporter)
         "Insert custom inline css to automatically set the
 background of code to whatever theme I'm using's background"
         (when (and (not blog/org-inline-css-hook-called) (eq exporter 'html))
-              (let* ((my-pre-bg (face-background 'default))
-                     (my-pre-fg (face-foreground 'default)))
-                (setq blog/org-inline-css-hook-called t)
-                (setq
-                 org-html-head-extra
-                 (concat
-                  org-html-head-extra
-                  (format "<style type=\"text/css\">\n pre.src {background-color: %s; color: %s;}</style>\n"
-                          my-pre-bg my-pre-fg)))))))
+          (let* ((my-pre-bg (face-background 'default))
+                 (my-pre-fg (face-foreground 'default)))
+            (setq blog/org-inline-css-hook-called t)
+            (setq
+             org-html-head-extra
+             (concat
+              org-html-head-extra
+              (format "<style type=\"text/css\">\n pre.src {background-color: %s; color: %s;}</style>\n"
+                      my-pre-bg my-pre-fg)))))))
     :config
     (progn
       (setq op/repository-directory (expand-file-name "~/projects/blog/"))
@@ -83,7 +69,26 @@ background of code to whatever theme I'm using's background"
                      :uri-generator op/generate-uri
                      :uri-template "/cv/"
                      :sort-by :date
-                     :category-index nil)))
+                     :category-index nil))
+
+      (defun op/render-content (&optional template param-table)
+        "My patched version of op/render-content to use org as a backend for css."
+        (mustache-render
+         (op/get-cache-create
+          (if template
+              (intern (replace-regexp-in-string "\\.mustache$" "-template" template))
+            :post-template)
+          (message (concat "Read " (or template "post.mustache") " from file"))
+          (file-to-string (concat op/template-directory
+                                  (or template "post.mustache"))))
+         (or param-table
+             (ht ("title" (or (op/read-org-option "TITLE") "Untitled"))
+                 ("content" (org-export-as 'html nil nil nil nil))))))
+
+      (defadvice op/do-publication (before blog/ensure-lang-syntax-loaded activate)
+        ;; Trigger load of language configurations
+        (load-ess-on-demand)
+        (require 'python)))
 
     (defun op/verify-git-repository (repo-dir)
       "This function will verify whether REPO-DIR is a valid git repository.
