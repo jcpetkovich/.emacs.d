735bab97791712a9026ad1fd6fb03766ba1e3bca
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2015-09-08 10:36:17 -0400

    Add snippets for R-mode.

---
+++ snippets/R-mode/.yas-compiled-snippets.el
@@ -0,0 +1,9 @@
+;;; Compiled snippets and support files for `R-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'R-mode
+                     '(("srcl" "source_local <- function(fname){\n    argv <- commandArgs(trailingOnly = FALSE)\n    base_dir <- dirname(substring(argv[grep(\"--file=\", argv)], 8))\n    if (length(base_dir)) { source(paste(base_dir, fname, sep=\"/\")) }\n    else {cat(\"ERROR: Could not source, running in a repl.\\n\")}\n}" "srcl" nil nil nil nil nil nil)
+                       ("f" "function(${1:args}) {$0}" "function" nil nil nil nil nil nil)))
+
+
+;;; Do not edit! File generated at Tue Sep  1 10:57:44 2015
