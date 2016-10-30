191f84ebd3e5a8b3b2a35c8b1a7c653a9ba33f27
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2016-10-30 14:19:05 -0400

    Prefer Erc

--- layers/irc-config/packages.el
+++ layers/irc-config/packages.el
@@ -12,153 +12,15 @@
 
 (setq irc-config-packages
 	'(
-		rcirc
-		rcirc-notify
-		rcirc-color
+    znc
 		)
 	)
 
 (setq irc-config-excluded-packages '())
 
-(defun personal/slack-connect ()
-	(interactive)
-	(let* ((host "embeddedsoftwaregroup.irc.slack.com")
-				 (port 6697)
-				 (user-name "jcp")
-				 (nick "jcp")
-				 (full-name "Jean-Christophe Petkovich")
-				 (password (funcall (plist-get (car (auth-source-search :host "embeddedsoftwaregroup.irc.slack.com"
-																																:port "irc")) :secret)))
-				 (encryption 'tls))
-		(rcirc-connect host port nick user-name full-name '() password encryption))
-	(let* ((host "acertateam.irc.slack.com")
-				 (port 6697)
-				 (user-name "jcp")
-				 (nick "jcp")
-				 (full-name "Jean-Christophe Petkovich")
-				 (password (funcall (plist-get (car (auth-source-search :host "acertateam.irc.slack.com"
-																																:port "irc")) :secret)))
-				 (encryption 'tls))
-		(rcirc-connect host port nick user-name full-name '() password encryption)))
+(setq-default erc-server-list
+              '(("chat.freenode.net"
+                 :port 6667
+                 :nick "Kruppe")))
 
-(defun irc-config/post-init-rcirc ()
-	(use-package rcirc
-		:commands rcirc
-		:defer t
-		:init
-		(progn
-			(add-to-list 'evil-normal-state-modes 'rcirc-mode)
-			(if (file-exists-p "~/.authinfo.gpg")
-
-					(progn
-						(require 'auth-source)
-						(setq-default
-						 irc-user-name (plist-get (car (auth-source-search :port '("nickserv"))) :user)
-						 rcirc-fill-column 'frame-width
-
-						 rcirc-server-alist
-						 `(
-							 ;; ("eyolfson.ca"
-							 ;;  :port 6697
-							 ;;  :encryption tls
-							 ;;  :auth "jcp")
-							 ("ptk.io"
-								:host "ptk.io"
-								:auth "jcp"
-								:port 6667))
-
-						 rcirc-home-server-alist
-						 `(
-							 ;; ("eyolfson.ca"
-							 ;;  :port 6697
-							 ;;  :encryption tls
-							 ;;  :auth "jcp")
-							 ("192.168.2.16"
-								:host "192.168.2.16"
-								:port 6667
-								:auth "jcp"))
-
-						 ;; Configure plugins
-						 rcirc-notify-timeout 0
-						 rcirc-notify-check-frame t
-
-						 ;; Configure
-						 rcirc-buffer-maximum-lines 2048))
-				(message "Warning: no ~/.authinfo type file found")))
-		:config
-		(progn
-			(require 's)
-
-			(defun-rcirc-command reconnect (arg)
-				"Reconnect the server process."
-				(interactive "i")
-				(if (buffer-live-p rcirc-server-buffer)
-						(with-current-buffer rcirc-server-buffer
-							(let ((reconnect-buffer (current-buffer))
-										(server (or rcirc-server rcirc-default-server))
-										(port (if (boundp 'rcirc-port) rcirc-port rcirc-default-port))
-										(nick (or rcirc-nick rcirc-default-nick))
-										channels)
-								(dolist (buf (buffer-list))
-									(with-current-buffer buf
-										(when (equal reconnect-buffer rcirc-server-buffer)
-											(remove-hook 'change-major-mode-hook
-																	 'rcirc-change-major-mode-hook)
-											(let ((server-plist (cdr (assoc-string server rcirc-server-alist))))
-												(when server-plist
-													(setq channels (plist-get server-plist :channels))))
-											)))
-								(if process (delete-process process))
-								(rcirc-connect server port nick
-															 nil
-															 nil
-															 channels)))))
-			;; This might be potentially useful
-			(defun rcirc-target-buffer-visible (proc sender response target text)
-				"Alternative for checking if a target buffer is visible"
-				(-any? (lambda (buffer) (equal buffer (rcirc-target-buffer proc sender response target text)))
-							 (rcirc-visible-buffers)))
-
-			;; TODO: Use an rcirc markup function for otr cleanup instead
-			(defun strip-otr-prefix (text)
-				(s-replace "05" "" text))
-
-			(defadvice rcirc-print (around rcirc-print-filter activate)
-				(let* ((text (ad-get-arg 4))
-							 (new-text (strip-otr-prefix text)))
-					(ad-set-arg 4 new-text)
-					ad-do-it))
-
-			;; Show where t.co urls are pointing
-			(defun t.co-resolve (url)
-				"Resolve t.co URL by launching `curl --head' and parsing the result."
-				(assert (string-match (concat "^" rcirc-url-regexp "$") url))
-				(let* ((curl (shell-command-to-string (format "curl --silent --head %s" url)))
-							 (location (when (and (string-match "\\`HTTP/1\.1 301" curl)
-																		(string-match (concat "^Location: "
-																													rcirc-url-regexp) curl))
-													 (match-string 1 curl))))
-					(or location url)))
-
-			(defun t.co-url-replace (sender response)
-				"Call `t.co-resolve' on every matching URL."
-				(let ((re "http://t\\.co/[a-z0-9]+"))
-					(while (re-search-forward re nil t)
-						(replace-match (save-match-data (t.co-resolve (match-string 0)))))))
-
-			(add-to-list 'rcirc-markup-text-functions
-									 't.co-url-replace)
-
-			(require 'rcirc-notify))))
-
-
-(defun irc-config/post-init-rcirc-notify ()
-	(use-package rcirc-notify
-		:defer t
-		:config (rcirc-notify-add-hooks)))
-
-(defun irc-config/post-init-rcirc-color ()
-	(use-package rcirc-color
-		:defer t
-		:config
-		(setq-default rcirc-color-is-deterministic t)))
+(setq erc-prompt-for-nickserv-password nil)
