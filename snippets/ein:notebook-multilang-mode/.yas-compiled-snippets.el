999c05c256e4a21fb8a48cd47638eb091fbf68e9
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2017-05-30 19:04:43 -0400

    Another compiled snippet.

---
+++ snippets/ein:notebook-multilang-mode/.yas-compiled-snippets.el
@@ -0,0 +1,8 @@
+;;; Compiled snippets and support files for `ein:notebook-multilang-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'ein:notebook-multilang-mode
+                     '(("retina" "%matplotlib inline\n%config InlineBackend.figure_format = 'retina'" "retina" nil nil nil "/home/jcp/.spacemacs.d/snippets/ein:notebook-multilang-mode/retina.yasnippet" nil nil)))
+
+
+;;; Do not edit! File generated at Mon May  8 21:29:13 2017
