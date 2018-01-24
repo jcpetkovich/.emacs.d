3719f1f78c8eb190a8d508b1ed945bb93bedd8f0
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2018-01-24 16:27:09 -0500

    Include org snippets.

---
+++ snippets/org-mode/.yas-compiled-snippets.el
@@ -0,0 +1,8 @@
+;;; Compiled snippets and support files for `org-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'org-mode
+                     '(("src" "#+BEGIN_SRC $1 :tangle yes\n$0\n#+END_SRC" "src" nil nil nil "/home/jcp/.spacemacs.d/snippets/org-mode/src.yasnippet" nil nil)))
+
+
+;;; Do not edit! File generated at Wed Dec 13 16:35:20 2017
