9f6beca95e89fe004a6195334b5f54e706b7139e
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2018-04-17 12:06:49 -0400

    Scroll down in inferior ess mode.

--- layers/personal/packages.el
+++ layers/personal/packages.el
@@ -440,6 +440,10 @@ an item line."
 
         (evil-declare-key 'normal inferior-ess-mode-map
           (kbd "C-d") 'evil-scroll-down)
+
+        (evil-declare-key 'visual inferior-ess-mode-map
+          (kbd "C-d") 'evil-scroll-down)
+
         (evil-declare-key 'normal ess-help-mode-map
           (kbd "Q") 'ess-help-quit
           (kbd "q") 'ess-help-quit)
