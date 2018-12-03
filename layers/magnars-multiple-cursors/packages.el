8892aa0f9a82032d26d86340ed84f3654bd0628b
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2018-12-03 12:34:03 -0500

    Avoid clashes between mc and mmc layers.

--- layers/magnars-multiple-cursors/packages.el
+++ layers/magnars-multiple-cursors/packages.el
@@ -23,8 +23,8 @@
     )
   )
 
-;; For each package, define a function multiple-cursors/init-<package-multiple-cursors>
-(defun multiple-cursors/init-multiple-cursors ()
+;; For each package, define a function magnars-multiple-cursors/init-<package-multiple-cursors>
+(defun magnars-multiple-cursors/init-multiple-cursors ()
   "Initialize multiple cursors"
   (use-package mc-mark-more
     :commands (mc/region-strings
@@ -42,15 +42,15 @@
       (require 'mc-mark-more)
       (bind-keys :map mc/keymap
            ("M-y" . yank-pop))
-      (multiple-cursors/enable-compat))))
+      (magnars-multiple-cursors/enable-compat))))
 
-(defun multiple-cursors/post-init-multiple-cursors ()
+(defun magnars-multiple-cursors/post-init-multiple-cursors ()
   (use-package multiple-cursors
     :defer t
     :init
     (bind-keys
-     ("M-m" . multiple-cursors/expand-or-mark-next-symbol)
-     ("M-M" . multiple-cursors/expand-or-mark-next-word)
+     ("M-m" . magnars-multiple-cursors/expand-or-mark-next-symbol)
+     ("M-M" . magnars-multiple-cursors/expand-or-mark-next-word)
      ("M-'" . mc/mark-all-dwim)
      ("C-S-n" . mc/mmlte--down)
      ("C-S-p" . mc/mmlte--up)
@@ -58,7 +58,7 @@
      ("C-S-b" . mc/mmlte--left))))
 
 
-(defun multiple-cursors/post-init-expand-region ()
+(defun magnars-multiple-cursors/post-init-expand-region ()
   (use-package expand-region
     :commands (er/mark-word er/mark-symbol)))
 
